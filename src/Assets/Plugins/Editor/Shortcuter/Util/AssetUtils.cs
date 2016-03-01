cb712d4aaefbd85d85f6b44a59d53a059fffb5c2
André Intentor Martins <andre@intentor.com.br>
2016-03-01 01:00:30 -0300

    Fix editor default resources folder creation

--- src/Assets/Plugins/Editor/Shortcuter/Util/AssetUtils.cs
+++ src/Assets/Plugins/Editor/Shortcuter/Util/AssetUtils.cs
@@ -10,17 +10,25 @@ namespace Intentor.Shortcuter.Util {
 	/// Utility class for assets.
 	/// </summary>
 	public static class AssetUtils {
-		/// <summary>Shortcut data path.</summary>
-		private const string SHORTCUT_DATA_PATH = "Editor Default Resources/Shortcuts.asset";
+		/// <summary>Editor default resources path.</summary>
+		private const string EDITOR_DEFAULT_RESOURCES = "Editor Default Resourcest";
+		/// <summary>Shortcuts data path.</summary>
+		private const string SHORTCUTS_DATA_PATH = EDITOR_DEFAULT_RESOURCES + "/Shortcuts.asset";
 
 		/// <summary>
 		/// Loads shortcuts data
 		/// </summary>
 		/// <returns>The shortcut data.</returns>
 		public static ShortcutData LoadShorcutData() {
-			var path = Path.Combine(Application.dataPath, SHORTCUT_DATA_PATH);
+			//Checks whether editor default resources path exists.
+			var resourcesPath = Path.Combine(Application.dataPath, EDITOR_DEFAULT_RESOURCES);
+			if (!Directory.Exists(resourcesPath)) {
+				Directory.CreateDirectory(resourcesPath);
+			}
 
-			if (!File.Exists(path)) {
+			//Checks whether the shourtcuts data path exists.
+			var dataPath = Path.Combine(Application.dataPath, SHORTCUTS_DATA_PATH);
+			if (!File.Exists(dataPath)) {
 				return CreateShortcutData();
 			} else {
 				return (ShortcutData)EditorGUIUtility.Load("Shortcuts.asset");
@@ -33,9 +41,10 @@ namespace Intentor.Shortcuter.Util {
 		/// <returns>The shortcut data.</returns>
 		public static ShortcutData CreateShortcutData() {
 			var shortcuts = ScriptableObject.CreateInstance<ShortcutData>();
-			var path = Path.Combine("Assets", SHORTCUT_DATA_PATH);
+			shortcuts.types = new List<ShortcutType>();
 
-			AssetDatabase.CreateAsset(shortcuts, path);
+			var dataPath = Path.Combine("Assets", SHORTCUTS_DATA_PATH);
+			AssetDatabase.CreateAsset(shortcuts, dataPath);
 			AssetDatabase.SaveAssets();
 
 			return shortcuts;
