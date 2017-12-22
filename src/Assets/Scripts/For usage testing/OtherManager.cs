22417d87411a78dd1d04a5d2999e3767993130f3
Sebastian Janus <sebastian.janus@fabrykagier.pl>
2017-12-22 15:17:34 +0100

    Added scripts and asset for testing purposes

---
+++ src/Assets/Scripts/For usage testing/OtherManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class OtherManager : ScriptableObject
+{
+    public string text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
